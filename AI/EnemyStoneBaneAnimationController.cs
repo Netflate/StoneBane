f8adeba1fb92fe9329bcc3cbc827bfecf25ffc88
Dhahbi Fadi <netflate@gmail.com>
2025-03-17 15:49:21 +0100

    Initial commit

---
+++ AI/EnemyStoneBaneAnimationController.cs
@@ -0,0 +1,80 @@
+﻿
+using System;
+using UnityEngine;
+
+namespace StoneBane.AI;
+
+public class EnemyPuppetAnimationController : MonoBehaviour
+{
+    [Header("References")]
+    public EnemyStoneBane controller;
+    public Animator animator;
+    
+    [Header("Particles")]
+    public ParticleSystem[] deathParticles;
+    
+    [Header("Sounds")]
+    [SerializeField] private Sound roamSounds;
+    [SerializeField] private Sound visionSounds;
+    [SerializeField] private Sound curiousSounds;
+    [SerializeField] private Sound hurtSounds;
+    [SerializeField] private Sound deathSound;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (controller.DeathImpulse)
+        {
+            controller.DeathImpulse = false;
+            animator.SetTrigger("Dies");
+        }
+
+        if (!controller.Enemy.IsStunned())
+        {
+            animator.SetFloat("Walking", EnemyUtil.GetAgentVelocity(EnemyUtil.GetEnemyNavMeshAgent(controller.Enemy)).magnitude);
+        }
+    }
+
+    public void SetDespawn()
+    {
+        EnemyUtil.GetEnemyParent(controller.Enemy).Despawn();
+    }
+
+    public void DeathParticlesImpulse()
+    {
+        ParticleSystem[] array = deathParticles;
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i].Play();
+        }
+    }
+    
+    public void PlayRoamSound()
+    {
+        roamSounds.Play(controller.Enemy.CenterTransform.position);
+    }
+    
+    public void PlayVisionSound()
+    {
+        visionSounds.Play(controller.Enemy.CenterTransform.position);
+    }
+    
+    public void PlayCuriousSound()
+    {
+        curiousSounds.Play(controller.Enemy.CenterTransform.position);
+    }
+    
+    public void PlayHurtSound()
+    {
+        hurtSounds.Play(controller.Enemy.CenterTransform.position);
+    }
+    
+    public void PlayDeathSound()
+    {
+        deathSound.Play(controller.Enemy.CenterTransform.position);
+    }
+}
---
+++ StoneBane.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using REPOLib.Modules;
+using UnityEngine;
+
+namespace StoneBane;
+
+[BepInPlugin("Netflate.StoneBane", "StoneBane", "1.0.0")]
+[BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
+public class StoneBane : BaseUnityPlugin
+{
+    internal static StoneBane Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger => Instance._logger;
+    private ManualLogSource _logger => base.Logger;
+    internal Harmony? Harmony { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        
+        // Prevent the plugin from being deleted
+        this.gameObject.transform.parent = null;
+        this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        Patch();
+
+        Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+    }
+
+    internal void Patch()
+    {
+        Harmony ??= new Harmony(Info.Metadata.GUID);
+        
+        Logger.LogInfo("Patching StoneBane...");
+        
+        Logger.LogInfo("Loading assets...");
+        LoadAssets();
+        
+        Harmony.PatchAll();
+    }
+
+    internal void Unpatch()
+    {
+        Harmony?.UnpatchSelf();
+    }
+    
+    private static void LoadAssets()
+    {
+        AssetBundle StoneBaneAssetBundle = LoadAssetBundle("StoneBane");
+
+        Logger.LogInfo("Loading StoneBane setup...");
+        EnemySetup StoneBaneSetup = StoneBaneAssetBundle.LoadAsset<EnemySetup>("Assets/REPO/Mods/plugins/StoneBane/Enemy - StoneBane.asset");
+        
+        Enemies.RegisterEnemy(StoneBaneSetup);
+        
+        Logger.LogDebug("Loaded StoneBane enemy!");
+    }
+    
+    public static AssetBundle LoadAssetBundle(string name)
+    {
+        Logger.LogDebug("Loading Asset Bundle: " + name);
+        AssetBundle bundle = null;
+        string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), name);
+        bundle = AssetBundle.LoadFromFile(path);
+        return bundle;
+    }
+}
